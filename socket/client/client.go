05fc82fb0ed8eb4c9c73939a1ded10e5a5e4a29a
yls <654498024@qq.com>
2019-05-16 10:54:39 +0800

    redis学习01

--- socket/client/client.go
+++ socket/client/client.go
@@ -2,14 +2,44 @@ package main
 import (
 	"net"
 	"fmt"
+	"bufio"
+	"os"
 )
+
+func readContent(conn net.Conn) {
+	defer conn.Close()
+	for {
+		content := make([]byte,1024)
+		n, err := conn.Read(content)
+		if err != nil {
+			fmt.Println("conn.Read err",err)
+			return 
+		}
+		fmt.Printf("\n %v",string(content[:n]))
+	}
+}
 
 func main() {
 	conn, err := net.Dial("tcp", "192.168.14.5:8080")
 	if err != nil {
-		// handle error
+		fmt.Println("与服务器链接失败 err",err)
 	}
-	fmt.Println(conn, "GET / HTTP/1.0\r\n\r\n")
+	content := os.Stdin
+	buf := bufio.NewReader(content)
+	for {
+		ln, readErr := buf.ReadString('\n')
+		if  readErr != nil {
+			fmt.Println("buf.ReadString err ",readErr)
+		}
+		fmt.Println("ln",ln)
+		n, err := conn.Write([]byte(ln))
+		if err != nil {
+			fmt.Println("conn.Write err ",err)
+		}
+		fmt.Printf("写入字节 %d\n",n)
+	}
+	go readContent(conn)
+	//fmt.Println(conn, "GET / HTTP/1.0\r\n\r\n")
 	//status, err := bufio.NewReader(conn).ReadString('\n')
 // ...
 }
--- socket/server/server.go
+++ socket/server/server.go
@@ -4,6 +4,25 @@ import (
 	"net"
 )
 
+var (
+	tcp = make([]net.Conn,0)
+)
+
+func handleConnection(conn net.Conn,conn2 net.Conn) {
+	defer conn.Close()
+	for {
+		buf := make([]byte,1024)
+		n, err := conn.Read(buf)
+		if err != nil {
+			fmt.Println("conn.Read err",err)
+			return 
+		}
+		fmt.Printf("\n %v : %v",conn.RemoteAddr().String(),string(buf[:n]))
+		conn2.Write([]byte(conn.RemoteAddr().String() + ": hello"))
+	}
+	
+}
+
 func main() {
 	ln, err := net.Listen("tcp", "0.0.0.0:8080")
 	if err != nil {
@@ -17,7 +36,17 @@ func main() {
 			continue
 		}else{
 			fmt.Println("连接成功.......",conn.RemoteAddr().Network(),conn.RemoteAddr().String())
+			tcp = append(tcp,conn)
+			fmt.Println(tcp)
 		}
-		//go handleConnection(conn)
+
+		var tcpc net.Conn
+
+		if len(tcp) > 1 {
+			tcpc = tcp[1]
+		}else{
+			tcpc = tcp[0]
+		}
+		go handleConnection(conn,tcpc)
 	}
 }
